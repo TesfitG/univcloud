81dea8c759146acb4db583fcb3829021326c12b6
TesfitG <tesgidey12@gmail.com>
2018-01-11 10:20:09 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,410 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Univ Cloud</title>
+
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">		
+		<link href="https://fonts.googleapis.com/css?family=Istok+Web" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	</head>
+
+	<body>
+		<!-- Wrapper -->
+		<section class="head" id="header">
+			<div class="wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<header class="header">
+								<div class="inner">
+									<!-- Logo -->
+									<a href="#menu"class="nav-bars">
+										<img src="images/nav_icon.svg" class="img-responsive" alt="Collapsable Navbar">
+									</a>
+									<a href="index.html" class="logo">
+										<span class="symbol"><img src="images/logo.png" alt="Title" /></span><span class="title">Univ-Cloud</span>
+									</a>
+								</div>
+							</header>
+							<!-- Menu -->
+							<nav id="menu">
+								<h2>Menu</h2>
+								<ul>
+									<li><a href="index.html">Home</a></li>
+									<li><a href="elements.html">Elements</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Main -->
+
+		<section class="main-body">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10">
+						<p class="drescription">A web portal hosted on a common <span class="orange"> Cloud Platform </span> for all universities in Ethiopia. Applications are developed and maintained from the same place and can be accessed across all universities. Each university manages its own private data</p>
+					</div>
+				</div>
+				<div class="tiles">
+					<div class="row">
+					<div class="col-md-3">
+							<div class="a">
+								<a href="#test-popup-1" class="open-popup-link">
+									<img src="images/astu.png" class="img-responsive" alt="Responsive image">
+									<div class="img-hover glass">
+										<div class="c-table">
+											<div class="ct-cell">
+												<h3 class="img-title">ASTU</h3>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div id="test-popup-1" class="white-popup mfp-hide">
+							  	<div class="container-fluid">
+									<div class="row">
+										<div class="pop-up-color">
+											<div class="col-md-4 p-l-0 p-r-0">
+												<img src="images/astu.png" class="img-responsive" alt="Responsive image">
+											</div>
+											<div class="col-md-4">
+												<div>
+													<h2 class="popup-head">ASTU</h2>
+												</div>
+												
+												 <div class="list-group">
+													<a href="http://localhost/UMS_new/UMS?univ=astu" class="list-group-item">University Management</a>
+													<a href="http://localhost/THM" class="list-group-item">Thesis Management</a>
+												  </div> 
+												
+												
+												<div class="pop-up-icon">
+													<div class="row">
+														<ul class="text-center popup-social-contact">
+															<li class="popup-social-icons">
+															<a href="#" class="icon pop-up fa-facebook"><span class="label">Facebook</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-twitter"><span class="label">Twitter</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-google-plus"><span class="label">GooglePlus</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-pinterest"><span class="label">pinterest</span></a>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="a">
+								<a href="#test-popup-2" class="open-popup-link">
+									<img src="images/aaulogo.png"   class="img-responsive" alt="Responsive image">
+									<div class="img-hover glass">
+										<div class="c-table">
+											<div class="ct-cell">
+												<h3 class="img-title">AAU</h3>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div id="test-popup-2" class="white-popup mfp-hide">
+							  	<div class="container-fluid">
+									<div class="row">
+										<div class="pop-up-color">
+											<div class="col-md-4 p-l-0 p-r-0">
+												<img src="images/aaulogo.png" class="img-responsive" alt="Responsive image">
+											</div>
+											<div class="col-md-4">
+												<div>
+													<h2 class="popup-head">AAU</h2>
+												</div>
+												<div class="list-group">
+													<a href="http://localhost/UMS_new/UMS?univ=aau" class="list-group-item">University Management</a>
+													<a href="http://localhost/THM" class="list-group-item">Thesis Management</a>
+												  </div> 
+												
+												
+												<div class="pop-up-icon">
+													<div class="row">
+														<ul class="text-center popup-social-contact">
+															<li class="popup-social-icons">
+															<a href="#" class="icon pop-up fa-facebook"><span class="label">Facebook</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-twitter"><span class="label">Twitter</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-google-plus"><span class="label">GooglePlus</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-pinterest"><span class="label">pinterest</span></a>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-3">
+							<div class="a">
+								<a href="#test-popup-3" class="open-popup-link">
+									<img src="images/aastulogo.jpg"   class="img-responsive" alt="Responsive image">
+									<div class="img-hover glass">
+										<div class="c-table">
+											<div class="ct-cell">
+												<h3 class="img-title">AASTU</h3>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div id="test-popup-3" class="white-popup mfp-hide">
+							  	<div class="container-fluid">
+									<div class="row">
+										<div class="pop-up-color">
+											<div class="col-md-4 p-l-0 p-r-0">
+												<img src="images/aastulogo.jpg" class="img-responsive" alt="Responsive image">
+											</div>
+											<div class="col-md-4">
+												<div>
+													<h2 class="popup-head">AASTU</h2>
+												</div>
+												<div class="list-group">
+													<a href="http://localhost/UMS_new/UMS?univ=aastu" class="list-group-item">University Management</a>
+													<a href="http://localhost/THM" class="list-group-item">Thesis Management</a>
+												  </div> 
+												<div class="pop-up-icon">
+													<div class="row">
+														<ul class="text-center popup-social-contact">
+															<li class="popup-social-icons">
+															<a href="#" class="icon pop-up fa-facebook"><span class="label">Facebook</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-twitter"><span class="label">Twitter</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-google-plus"><span class="label">GooglePlus</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-pinterest"><span class="label">pinterest</span></a>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="a">
+								<a href="#test-popup-4" class="open-popup-link">
+									<img src="images/dulogo.png" class="img-responsive" alt="Responsive image">
+									<div class="img-hover shoes-glass">
+										<div class="c-table">
+											<div class="ct-cell">
+												<h3 class="img-title">DU</h3>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div id="test-popup-4" class="white-popup mfp-hide">
+							  	<div class="container-fluid">
+									<div class="row">
+										<div class="pop-up-color">
+											<div class="col-md-4 p-l-0 p-r-0">
+												<img src="images/dulogo.png" class="img-responsive" alt="Responsive image">
+											</div>
+											<div class="col-md-4">
+												<div>
+													<h2 class="popup-head">DU</h2>
+												</div>
+												<div class="list-group">
+													<a href="http://localhost/UMS_new/UMS?univ=du" class="list-group-item">University Management</a>
+													<a href="http://localhost/THM" class="list-group-item">Thesis Management</a>
+												  </div> 
+												
+												<div class="pop-up-icon">
+													<div class="row">
+														<ul class="text-center popup-social-contact">
+															<li class="popup-social-icons">
+															<a href="#" class="icon pop-up fa-facebook"><span class="label">Facebook</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-twitter"><span class="label">Twitter</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-google-plus"><span class="label">GooglePlus</span></a>
+															</li>
+															<li class="popup-social-icons">
+																<a href="#" class="icon pop-up fa-pinterest"><span class="label">pinterest</span></a>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div><br>
+					
+			</div>
+		</section>
+
+		<section class="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>GET IN TOUCH</h1>
+							<form>
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<input type="text" class="form-control gt" name="name" id="name" placeholder="Name"/>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<input type="email" class="form-control gt" name="email" id="email" placeholder="Email" />
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-12">
+										<div class="from-group">
+											<textarea class="form-control gt" rows="2" name="message" id="message" placeholder="Message..."></textarea>
+										</div>
+									</div>
+								</div>
+							</form>
+							<div class="form-group"  align="right">
+							    <div class="col-sm-offset-2 col-sm-10">
+							      <a href="#" type="submit" class="btn btn-tobais btn-send-2">SEND</a>
+							    </div>
+							</div>
+						</div>
+					<div class="col-md-4">
+						<h1>CONTACT INFO</h1>
+							<p class="contact-desc">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus voluptatum necessi tatibus.
+							</p>
+							<p><Strong>Address: </Strong> Block 508, Adama, Ethiopia</p>
+							<p><Strong>Call:</Strong> +251 913 21 57 34</p>						
+							<p><Strong>Fax: </Strong>+251 22331133</p>						
+							<p><Strong>Email: </Strong>tesgidey@gmail.com</p>						
+					</div>
+				</div>				
+			</div>
+		</section>
+
+		<section class="social-contact-icon">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-xs-12">
+						<ul class="text-center social-contact">
+							<li class="social-icons">
+							<a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-google-plus"><span class="label">GooglePlus</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-pinterest"><span class="label">pinterest</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-stumbleupon"><span class="label">Stumbleupon</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-tumblr"><span class="label">tumblr</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-reddit-alien"><span class="label">reddit-alien</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-dribbble"><span class="label">Dribbble</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-behance"><span class="label">Behance</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-linkedin"><span class="label">Linkedin</span></a>
+							</li>
+							<li class="social-icons">
+								<a href="#" class="icon style2 fa-vk"><span class="label">vk</span></a>
+							</li>
+						</ul>							
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="footer">		
+			<footer>
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-8 col-xs-9">
+							<p class="right-color">&copy; Copyright 2016. All rights reserved by <a href="https://themewagon.com/" target="_blank">ThemeWagon</a></p>
+						</div>
+						<div class="col-sm-4 col-xs-3" align="right">
+							<a href="#" id="back-to-top" class="top text-right" >TOP <i class="fa fa-angle-up" aria-hidden="true"></i> </a>
+						</div>
+					</div>
+				</div>											
+			</footer>
+		</section>
+
+	<!-- Scripts -->
+
+		<script src="assets/js/jquery-3.1.0.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+		<script src="assets/js/script.js"></script>
+
+		<!--  ===== Scroll to Top ====  -->
+	    <script>
+			if ($('#back-to-top').length) {
+			    $('#back-to-top').on('click', function (e) {
+			        e.preventDefault();
+			        $('html,body').animate({
+			            scrollTop: 0
+			        }, 700);
+			    });
+			}
+		</script>
+		
+	</body>
+</html>
